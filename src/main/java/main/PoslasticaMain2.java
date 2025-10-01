3ea01d86b181a3cf2f4b73fa15c33b7049a5c821
jovanaajankovic <jankovicj297@gmail.com>
2025-10-02 01:24:57 +0200

    Dokumentovana klasa PoslasticaMain2

--- src/main/java/main/PoslasticaMain2.java
+++ src/main/java/main/PoslasticaMain2.java
@@ -6,8 +6,25 @@ import com.google.gson.Gson;
 import com.jovanaajankovic.poslasticarnica.Poslastica;
 import com.jovanaajankovic.poslasticarnica.Sastojak;
 
+/**
+ * Demonstrira ucitavanje (deserijalizaciju) objekta Poslastica
+ * iz JSON fajla kreiranog pomocu Gson biblioteke.
+ * 
+ * Ova klasa cita fajl poslastica.json, ucitava poslasticu sa njenim sastojcima
+ * i prikazuje ih na standardnom izlazu.
+ * 
+ * @author Jovana Jankovic
+ */
 public class PoslasticaMain2 {
 
+	/**
+     * Glavna metoda koja se izvrsava pri pokretanju programa.
+     * Ucitava se JSON fajl poslastica.json, deserijalizuje u objekat tipa
+     * Poslastica, a zatim se na konzoli ispisuju podaci o poslastici
+     * i njenim sastojcima.
+     *
+     * @param args argumenti komandne linije (ne koriste se u ovom primeru)
+     */
 	public static void main(String[] args) {
 		Gson gson = new Gson();
 
