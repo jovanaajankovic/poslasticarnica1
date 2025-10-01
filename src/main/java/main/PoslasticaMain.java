e7d3a6160b80541de25c4a9b2e689bb34165ae0c
jovanaajankovic <jankovicj297@gmail.com>
2025-10-02 01:22:04 +0200

    Dokumentovana klasa PoslasticaMain

--- src/main/java/main/PoslasticaMain.java
+++ src/main/java/main/PoslasticaMain.java
@@ -9,8 +9,24 @@ import com.jovanaajankovic.poslasticarnica.Poslastica;
 import com.jovanaajankovic.poslasticarnica.Sastojak;
 import com.jovanaajankovic.poslasticarnica.TipPoslastice;
 
+/**
+ * Demonstrira rad sa klasama Poslastica, TipPoslastice i Sastojak.
+ * U ovom primeru kreira se objekat poslastice sa sastojcima i serijalizuje u JSON format
+ * pomoću Gson biblioteke.
+ *
+ * JSON se snima u fajl poslastica.json.
+ *
+ * @author Jovana Jankovic
+ */
 public class PoslasticaMain {
 
+	/**
+     * Glavna metoda koja se izvrsava pri pokretanju programa.
+     * Kreira se tip poslastice, zatim poslastica sa sastojcima,
+     * i cuva se u JSON fajl koriscenjem Gson-a.
+     *
+     * @param args argumenti komandne linije (ne koriste se u ovom primeru)
+     */
 	public static void main(String[] args) {
 		TipPoslastice tip = new TipPoslastice(1L, "Torta");
         Poslastica p = new Poslastica(101L, "Cokoladna torta", 500, "Ukusna cokoladna torta", tip, new ArrayList<>());
