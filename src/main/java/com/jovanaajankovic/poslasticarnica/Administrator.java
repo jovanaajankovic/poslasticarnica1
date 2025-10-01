ffa42bd3ae9253887b3a003bd8865762b3ed133c
jovanaajankovic <jankovicj297@gmail.com>
2025-10-02 00:12:26 +0200

    Dokumentovana klasa Administrator

--- src/main/java/com/jovanaajankovic/poslasticarnica/Administrator.java
+++ src/main/java/com/jovanaajankovic/poslasticarnica/Administrator.java
@@ -2,15 +2,41 @@ package com.jovanaajankovic.poslasticarnica;
 
 import java.util.Objects;
 
+/**
+ * Predstavlja Administratora sa osnovnim podacima o identitetu i pristupu.
+ * <p>
+ * Svaki administrator ima jedinstveni ID, ime, prezime, username i password.
+ * </p>
+ * 
+ * @author Jovana Jankovic
+ * 
+ */
 public class Administrator {
 	
+	/** ID administratora kao Long */
 	private Long administratorID;
+	
+	/** Ime administratora kao String*/
     private String ime;
+    
+    /** Prezime administratora kao String*/
     private String prezime;
+    
+    /** Username adminsitratora kao String*/
     private String username;
+    
+    /** Password administratora kao String*/
     private String password;
     
-    
+    /**
+     * Inicijalizuje objekat klase Administrator sa svim parametrima.
+     * 
+     * @param administratorID Jedinstveni ID administratora. Mora biti pozitivan broj.
+     * @param ime Ime administratora. Ne sme biti null niti prazan string.
+     * @param prezime Prezime administratora. Ne sme biti null niti prazan string.
+     * @param username Username administratora. Ne sme biti null niti prazan string.
+     * @param password Password administratora. Mora imati najmanje 8 karaktera.
+     */
     public Administrator(Long administratorID, String ime, String prezime, String username, String password) {
         setAdministratorID(administratorID);
         setIme(ime);
@@ -19,16 +45,30 @@ public class Administrator {
         setPassword(password);
     }
     
-    
+    /**
+     * Inicijalizuje objekat klase Adminstrator sa atributima koji imaju default vrednosti.
+     */
     public Administrator() {
 		
 	}
     
-    
+    /**
+     * Vraca ID administratora.
+     * 
+     * @return administratorID
+     */
 	public Long getAdministratorID() {
 		return administratorID;
 	}
-
+	
+	/**
+     * Postavlja ID administratora.
+     * ID mora biti pozitivan broj i ne sme biti null.
+     * 
+     * @param administratorID ID administratora
+     * @throws java.lang.NullPointerException ako je ID null
+     * @throws java.lang.IllegalArgumentException ako je ID manji ili jednak nuli
+     */
 	public void setAdministratorID(Long administratorID) {
 		if (administratorID == null)
 			throw new NullPointerException("ID ne sme biti null.");
@@ -40,11 +80,23 @@ public class Administrator {
 		
 	}
 	
-
+	/**
+     * Vraca ime administratora.
+     * 
+     * @return ime
+     */
 	public String getIme() {
 		return ime;
 	}
 
+	/**
+     * Postavlja ime administratora.
+     * Ime ne sme biti null niti prazan string.
+     * 
+     * @param ime Ime administratora
+     * @throws java.lang.NullPointerException ako je ime null
+     * @throws java.lang.IllegalArgumentException ako je ime prazan string ili samo razmaci
+     */
 	public void setIme(String ime) {
 		if (ime == null)
 			throw new NullPointerException("Ime ne sme biti null.");
@@ -56,11 +108,23 @@ public class Administrator {
 		
 	}
 
-	
+	/**
+    * Vraca prezime administratora.
+    * 
+    * @return prezime
+    */
 	public String getPrezime() {
 		return prezime;
 	}
 
+	/**
+     * Postavlja prezime administratora.
+     * Prezime ne sme biti null niti prazan string.
+     * 
+     * @param prezime Prezime administratora
+     * @throws java.lang.NullPointerException ako je prezime null
+     * @throws java.lang.IllegalArgumentException ako je prezime prazan string ili samo razmaci
+     */
 	public void setPrezime(String prezime) {
 		if (prezime == null)
 			throw new NullPointerException("Prezime ne sme biti null.");
@@ -72,11 +136,23 @@ public class Administrator {
 		
 	}
 	
-
+	 /**
+     * Vraca username administratora.
+     * 
+     * @return username
+     */
 	public String getUsername() {
 		return username;
 	}
 
+	/**
+     * Postavlja username administratora.
+     * Username ne sme biti null niti prazan string.
+     * 
+     * @param username Username administratora
+     * @throws java.lang.NullPointerException ako je username null
+     * @throws java.lang.IllegalArgumentException ako je username prazan string
+     */
 	public void setUsername(String username) {
 		if (username == null)
 			throw new NullPointerException("Username ne sme biti null.");
@@ -88,11 +164,23 @@ public class Administrator {
 		
 	}
 	
-
+	/**
+     * Vraca password administratora.
+     * 
+     * @return password
+     */
 	public String getPassword() {
 		return password;
 	}
 
+	/**
+     * Postavlja password administratora.
+     * Password ne sme biti null i mora imati najmanje 8 karaktera.
+     * 
+     * @param password Password administratora
+     * @throws java.lang.NullPointerException ako je password null
+     * @throws java.lang.IllegalArgumentException ako je password kraci od 8 karaktera
+     */
 	public void setPassword(String password) {
 		if (password == null)
 			throw new NullPointerException("Password ne sme biti null.");
@@ -104,7 +192,17 @@ public class Administrator {
 		
 	}
 	
-
+	 /**
+     * Poredi dva administratora po njihovom ID-u.
+     * 
+     * @param obj Drugi objekat sa kojim se poredi
+     * @return 
+     * <ul>
+     *  <li> true ako je uneti objekat razlicit od null, ako je taj objekat klase Administrator
+     * 		 i ako je ID isti kao kod prvog Administratora </li>
+     *  <li> false u svim ostalim slucajevima </li>
+     * </ul>
+     */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -117,7 +215,11 @@ public class Administrator {
 		return Objects.equals(administratorID, other.administratorID);
 	}
 
-	
+	/**
+     * Vraca tekstualni prikaz administratora.
+     * 
+     * @return String sa podacima o imenu i prezimenu administratora u formatu ime + " " + prezime
+     */
 	@Override
 	public String toString() {
 		return ime + " " + prezime;
