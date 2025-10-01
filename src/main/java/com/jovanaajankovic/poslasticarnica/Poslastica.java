0d48188bf7fbb740bbbff14df9bde1a6644ab251
jovanaajankovic <jankovicj297@gmail.com>
2025-10-02 00:28:09 +0200

    Dokumentovana klasa Poslastica

--- src/main/java/com/jovanaajankovic/poslasticarnica/Poslastica.java
+++ src/main/java/com/jovanaajankovic/poslasticarnica/Poslastica.java
@@ -2,16 +2,45 @@ package com.jovanaajankovic.poslasticarnica;
 
 import java.util.ArrayList;
 
+/**
+* Predstavlja poslasticu koja se prodaje u poslasticarnici.
+* <p>
+* Svaka poslastica ima jedinstveni ID, naziv, cenu po komadu,
+* opis, tip poslastice i listu sastojaka. 
+* </p>
+* 
+* @author Jovana Jankovic
+*/
 public class Poslastica {
 	
+	/** ID poslastice kao Long */
 	private Long poslasticaID;
+	
+	/** Naziv poslastice kao String*/
     private String naziv;
+    
+    /** Cena po komadu poslastice kao double*/
     private double cenaPoKomadu;
+    
+    /** Opis poslastice kao String*/
     private String opis;
+    
+    /** Tip poslastice npr. kolac, torta...*/
     private TipPoslastice tipPoslastice;
+    
+    /** Lista sastojaka od kojih se pravi poslastica*/
     private ArrayList<Sastojak> sastojci;
 
-  
+    /**
+     * Inicijalizuje objekat klase Poslastica sa svim parametrima.
+     * 
+     * @param poslasticaID Jedinstveni ID poslastice. Mora biti pozitivan broj i ne sme biti null.
+     * @param naziv Naziv poslastice. Ne sme biti null ili prazan string.
+     * @param cenaPoKomadu Cena jedne poslastice. Mora biti veca od nule.
+     * @param opis Opis poslastice. Ne sme biti null niti prazan string.
+     * @param tipPoslastice Tip poslastice. Ne sme biti null.
+     * @param sastojci Lista sastojaka. Ne sme biti null niti prazna.
+     */
     public Poslastica(Long poslasticaID, String naziv, double cenaPoKomadu, String opis, TipPoslastice tipPoslastice, ArrayList<Sastojak> sastojci) {
         setPoslasticaID(poslasticaID);
         setNaziv(naziv);
@@ -22,16 +51,30 @@ public class Poslastica {
         
     }
 
-    
+    /**
+     * Inicijalizuje objekat klase Poslastica sa atributima koji imaju default vrednosti.
+     */
     public Poslastica() {
     	
     }
     
-    
+    /**
+     * Vraca ID poslastice.
+     * 
+     * @return ID poslastice
+     */
     public Long getPoslasticaID() {
 		return poslasticaID;
 	}
 
+    /**
+     * Postavlja ID poslastice.
+     * ID poslastice mora biti pozitivan broj i ne sme biti null.
+     * 
+     * @param poslasticaID ID poslastice 
+     * @throws java.lang.NullPointerException ako je ID null
+     * @throws java.lang.IllegalArgumentException ako je ID manji ili jednak nuli
+     */
 	public void setPoslasticaID(Long poslasticaID) {
 		if (poslasticaID == null)
 			throw new NullPointerException("ID ne sme biti null.");
@@ -43,11 +86,23 @@ public class Poslastica {
 		
 	}
 
-
+	/**
+     * Vraca naziv poslastice.
+     * 
+     * @return naziv poslastice
+     */
 	public String getNaziv() {
 		return naziv;
 	}
 
+	/**
+     * Postavlja naziv poslastice.
+     * Naziv ne sme biti null niti prazan string.
+     * 
+     * @param naziv Naziv poslastice
+     * @throws java.lang.NullPointerException ako je naziv null
+     * @throws java.lang.IllegalArgumentException ako je naziv prazan string ili sadrzi samo razmake
+     */
 	public void setNaziv(String naziv) {
 		if (naziv == null)
 			throw new NullPointerException("Naziv ne sme biti null.");
@@ -59,11 +114,22 @@ public class Poslastica {
 		
 	}
 
-
+	/**
+     * Vraca cenu po komadu poslastice.
+     * 
+     * @return cena po komadu
+     */
 	public double getCenaPoKomadu() {
 		return cenaPoKomadu;
 	}
 
+	/**
+     * Postavlja cenu po komadu poslastice.
+     * Cena mora biti veca od nule.
+     * 
+     * @param cenaPoKomadu Cena poslastice 
+     * @throws java.lang.IllegalArgumentException ako je cena manja ili jednaka nuli
+     */
 	public void setCenaPoKomadu(double cenaPoKomadu) {
 		if (cenaPoKomadu <= 0)
 	        throw new IllegalArgumentException("Cena po komadu mora biti veca od nule.");
@@ -72,11 +138,23 @@ public class Poslastica {
 		
 	}
 
-
+	/**
+     * Vraca opis poslastice.
+     * 
+     * @return opis poslastice
+     */
 	public String getOpis() {
 		return opis;
 	}
 
+	/**
+     * Postavlja opis poslastice.
+     * Opis ne sme biti null niti prazan string.
+     * 
+     * @param opis Opis poslastice 
+     * @throws java.lang.NullPointerException ako je opis null
+     * @throws java.lang.IllegalArgumentException ako je opis prazan string ili sadrzi samo razmake
+     */
 	public void setOpis(String opis) {
 		if (opis == null)
 			throw new NullPointerException("Opis ne sme biti null.");
@@ -88,11 +166,22 @@ public class Poslastica {
 		
 	}
 
-
+	/**
+     * Vraca tip poslastice.
+     * 
+     * @return tip poslastice
+     */
 	public TipPoslastice getTipPoslastice() {
 		return tipPoslastice;
 	}
 
+	/**
+     * Postavlja tip poslastice.
+     * Tip poslastice ne sme biti null.
+     * 
+     * @param tipPoslastice Tip poslastice
+     * @throws java.lang.NullPointerException ako je tip poslastice null
+     */
 	public void setTipPoslastice(TipPoslastice tipPoslastice) {
 		if (tipPoslastice == null) 
 	        throw new NullPointerException("Tip poslastice ne sme biti null.");
@@ -101,11 +190,23 @@ public class Poslastica {
 		
 	}
 
-
+	/**
+     * Vraca listu sastojaka poslastice.
+     * 
+     * @return lista sastojaka
+     */
 	public ArrayList<Sastojak> getSastojci() {
 		return sastojci;
 	}
 
+	/**
+     * Postavlja listu sastojaka.
+     * Lista sastojaka ne sme biti null niti prazna.
+     * 
+     * @param sastojci Lista sastojaka
+     * @throws java.lang.NullPointerException ako je lista null
+     * @throws java.lang.IllegalArgumentException ako je lista prazna
+     */
 	public void setSastojci(ArrayList<Sastojak> sastojci) {
 		if (sastojci == null)
 			throw new NullPointerException("Lista sastojaka ne sme biti null.");
@@ -117,7 +218,11 @@ public class Poslastica {
 		
 	}
 
-
+	/**
+     * Vraca tekstualni prikaz poslastice.
+     * 
+     * @return String sa podacima o nazivu i ceni poslastice u formatu naziv + " (Cena po komadu: " + cenaPoKomadu + "din)"
+     */
 	@Override
     public String toString() {
         return naziv + " (Cena po komadu: " + cenaPoKomadu + "din)";
