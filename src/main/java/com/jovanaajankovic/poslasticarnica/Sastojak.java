877389141d0e0219ec3675e0404c973ad896152c
jovanaajankovic <jankovicj297@gmail.com>
2025-10-02 00:54:19 +0200

    Dokumentovana klasa Sastojak

--- src/main/java/com/jovanaajankovic/poslasticarnica/Sastojak.java
+++ src/main/java/com/jovanaajankovic/poslasticarnica/Sastojak.java
@@ -1,11 +1,36 @@
 package com.jovanaajankovic.poslasticarnica;
 
+/**
+ * Predstavlja sastojak poslastice.
+ * <p>
+ * Svaki sastojak ima redni broj, naziv i pripada nekoj poslastici.
+ * </p>
+ *
+ * @author Jovana Jankovic
+ */
 public class Sastojak {
 	
+	/**
+     * Poslastica kojoj sastojak pripada.
+     * <p>
+     * Obelezena je kao <b>transient</b> da se ne bi serijalizovala.
+     * </p>
+     */
 	 private transient Poslastica poslastica;
+	 
+	 /** Redni broj sastojka kao int*/
 	 private int rb;
+	 
+	 /** Naziv sastojka kao string*/
 	 private String naziv;
 
+	 /**
+	  * Inicijalizuje objekat klase Sastojak sa svim parametrima.
+	  *
+	  * @param poslastica Poslastica kojoj sastojak pripada. Ne sme biti null.
+	  * @param rb Redni broj sastojka. Mora biti veci od nule.
+	  * @param naziv Naziv sastojka. Ne sme biti null niti prazan string.
+	  */
 	 public Sastojak(Poslastica poslastica, int rb, String naziv) {
 		 setPoslastica(poslastica);
 		 setRb(rb);
@@ -13,16 +38,29 @@ public class Sastojak {
 		 
 	 }
 	 
-	 
+	 /**
+	  * Inicijalizuje objekat klase Sastojak sa atributima koji imaju default vrednosti.
+	  */
 	 public Sastojak() {
 		 
 	 }
 
-
+	 /**
+	  * Vraca poslasticu kojoj sastojak pripada.
+	  *
+	  * @return poslastica
+	  */
 	 public Poslastica getPoslastica() {
 		 return poslastica;
 	 }
 
+	 /**
+	  * Postavlja poslasticu kojoj sastojak pripada.
+	  * Poslastica ne sme biti null.
+	  *
+	  * @param poslastica Poslastica 
+	  * @throws java.lang.NullPointerException ako je poslastica null
+	  */
 	 public void setPoslastica(Poslastica poslastica) {
 		 if (poslastica == null)
 		        throw new NullPointerException("Poslastica ne sme biti null.");
@@ -31,11 +69,22 @@ public class Sastojak {
 		 
 	 }
 
-
+	 /**
+	  * Vraca redni broj sastojka.
+	  *
+	  * @return redni broj
+	  */
 	 public int getRb() {
 		 return rb;
 	 }
 
+	 /**
+	  * Postavlja redni broj sastojka.
+	  * Redni broj mora biti veci od nule.
+	  *
+	  * @param rb Redni broj
+	  * @throws java.lang.IllegalArgumentException ako je redni broj negativan ili jednak nuli
+	  */
 	 public void setRb(int rb) {
 		 if (rb <= 0)
 		        throw new IllegalArgumentException("Redni broj mora biti pozitivan broj.");
@@ -44,11 +93,23 @@ public class Sastojak {
 		 
 	 }
 
-
+	 /**
+	  * Vraca naziv sastojka.
+	  *
+	  * @return naziv sastojka
+	  */
 	 public String getNaziv() {
 		 return naziv;
 	 }
 
+	 /**
+	  * Postavlja naziv sastojka.
+	  * Naziv ne sme biti null niti prazan string.
+	  *
+	  * @param naziv Naziv
+	  * @throws java.lang.NullPointerException ako je naziv null
+	  * @throws java.lang.IllegalArgumentException ako je naziv prazan ili sadrzi samo razmake
+	  */
 	 public void setNaziv(String naziv) {
 		 if (naziv == null)
 				throw new NullPointerException("Naziv ne sme biti null.");
@@ -60,7 +121,11 @@ public class Sastojak {
 		 
 	 }
 
-
+	 /**
+	  * Vraca tekstualni prikaz sastojka.
+	  * 
+	  * @return String sa podacima o rednom broju i nazivu sastojka u formatu rb + ". " + naziv
+	  */
 	 @Override
 	 public String toString() {
 		 return rb + ". " + naziv;
